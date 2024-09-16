f7bdb91a95fe01fff31949227e8e384884ef2dd7
Surya-Kumar-D <suryakumar.dhanasekar@gmail.com>
2024-09-16 17:51:51 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This is design is from kevin powell. I am building the website from scratch with my own code.
---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+function Header() {
+  return (
+    <div className="header">
+      <div className="header__container">
+        <div className="header__left">
+          <img src="man.jpg" alt="Profile picture" />
+        </div>
+        <div className="header__right">
+          <div className="header__right-1">Hi, I am</div>
+          <div className="header__right-2">Surya Kumar D</div>
+          <div className="header__right-3">Front-end dev</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
